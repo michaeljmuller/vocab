88f039d156e7eaa23bba8f76c159a2c5c76fc80c
Michael Muller <mike@themullers.org>
2024-11-26 09:33:22 -1000

    implement home page

--- src/main/java/org/themullers/vocab/Controller.java
+++ src/main/java/org/themullers/vocab/Controller.java
@@ -7,17 +7,29 @@ import org.springframework.web.servlet.ModelAndView;
 public class Controller {
 
     Vocabulary vocabulary;
+    ProgressPersister progressPersister;
 
-    public Controller(Vocabulary vocabulary) {
+    public Controller(Vocabulary vocabulary, ProgressPersister progressPersister) {
         this.vocabulary = vocabulary;
+        this.progressPersister = progressPersister;
     }
 
-    @GetMapping("/")
-    public ModelAndView home() {
-        var mv = new ModelAndView("home");
+    @GetMapping("/spanish-to-english")
+    public ModelAndView spanishToEnglish() {
+        var mv = new ModelAndView("spanish-to-english");
         mv.addObject("vocab", vocabulary.getEntries());
         mv.addObject("spanishWords", vocabulary.getSpanishWords());
         mv.addObject("englishWords", vocabulary.getEnglishWords());
         return mv;
     }
+
+    @GetMapping("/")
+    public ModelAndView home() {
+        var mv = new ModelAndView("home");
+        mv.addObject("wordCount", vocabulary.getEntries().size());
+        mv.addObject("expandedSpanishWordCount", vocabulary.getSpanishWords().size());
+        mv.addObject("spanishWordsLearned", progressPersister.getProgress().getSpanishWordsLearned().size());
+        return mv;
+    }
+
 }
